177327f0cd9b29e89eb12b7f67b7921b8222f3eb
Parryhip <samuel.andelin@ucas-edu.net>
2024-09-04 09:51:48 -0600

    Who are you first commit.

---
+++ whoAreYou/main.py
@@ -0,0 +1,9 @@
+#Samuel Andelin, Who are you?
+
+#inputs for user details
+name = input("What is your name?: ")
+age = input("What is your age?: ")
+color = input("What is your favorite color?: ")
+
+#print statement for all user details
+print("Hello " + name + "! You are " + age + " years old. Your favorite color is " + color + ". That is a good choice!")
