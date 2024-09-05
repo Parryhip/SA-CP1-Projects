ae752c861181c96e4494e54724de61101de7efa5
Parryhip <samuel.andelin@ucas-edu.net>
2024-09-05 09:31:55 -0600

    tables for schol ceremony

---
+++ schoolAwardsCeremonyTables/main.py
@@ -0,0 +1,10 @@
+#Samuel Andelin, Setting, Resetting, and Resetting
+staff = 32
+students = 100
+students -= 1
+staff -= 3
+guests = students *2
+guests -= 15
+staff += 1
+tableSize = 12
+print((staff + students + guests)/tableSize)
