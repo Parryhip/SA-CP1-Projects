75702c609a610a057e53576e71c452253fda0ac4
Parryhip <samuel.andelin@ucas-edu.net>
2024-09-05 09:39:44 -0600

    celsius to fahrenheit

---
+++ celsiusToFahrenheit/main.py
@@ -0,0 +1,4 @@
+#Samuel Andelin, Celsius to Fahrenheit
+celsius = input("How many degrees Celsius?: ")
+fahrenheit = (int(celsius) * (9/5))+32
+print("Your temperature of "+str(celsius)+" to Fahrenheit is "+str(fahrenheit))
