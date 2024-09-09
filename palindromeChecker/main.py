e8a8589b87f233a79556e5f0fbd959b36603af57
Parryhip <samuel.andelin@ucas-edu.net>
2024-09-09 09:44:34 -0600

    Palindrome practice.

---
+++ palindromeChecker/main.py
@@ -0,0 +1,12 @@
+#Samuel Andelin, Palindrome
+palindrome = input("Enter a string to check if it is a palindrome: ")
+list = []
+for i in palindrome:
+    list.append(i)
+revlist = reversed(list)
+rev = "".join(revlist)
+
+if str(rev) == str(palindrome):
+    print("This is a palindrome!")
+else:
+    print("This isn't a palidrome.")
