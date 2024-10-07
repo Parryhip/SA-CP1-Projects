cf5f3cfc947eeb1c1f256fff9a97676bb4dd36dc
Parryhip <samuel.andelin@ucas-edu.net>
2024-10-07 09:54:24 -0600

    some comments made

--- numberGuessingGame/main.py
+++ numberGuessingGame/main.py
@@ -1,9 +1,15 @@
 #Samuel Andelin, Number Guessing Game
+#get random database
 import random
+#chooses random number
 numtoguess = random.randint(1,100)
+#user instructions
 print("I'm thinking of a number between 1 and 100. Try to guess it.")
+#sets that the user hasn't guessed the number yet
 userguessedit = False
+#while loop until the user guesses the number
 while userguessedit == False:
+    #user prompt for number
     userguess = input("What is your guess for the number?: ")
     userguessint = int(userguess)
     if userguessint < numtoguess:
