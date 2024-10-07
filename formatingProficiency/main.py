78ab66fdf4fa932ea514b34f500719ea984b8bac
Parryhip <samuel.andelin@ucas-edu.net>
2024-10-07 09:19:27 -0600

    fixed percentage display

--- formatingProficiency/main.py
+++ formatingProficiency/main.py
@@ -1,9 +1,10 @@
 #Samuel Andelin, What are these numbers? 
+import math
 usernum = input("Input a number to have commas in-between every thousand, \nbe rounded to the nearest ten thousandth, \nbe a percentage, \nand be a dollar amount.")
 commasint = f"{float(usernum):,}"
 print("\nYour number with commas is "+ str(commasint))
 print("\nYour number rounded to the nearest ten thousand is "+ str(round(float(usernum), 4)))
-percentagenum = float(usernum)*100
+percentagenum = int(float(usernum))*100
 print("\nYour number as a percentage is "+str(percentagenum)+" %")
 dollarnum = "$" + str(round(float(usernum), 2))
 print("\nYour number as a dollar amount is "+dollarnum)
