becb5aba3eb00ac9004f21196e6f00072071a17a
Parryhip <samuel.andelin@ucas-edu.net>
2024-10-23 09:44:10 -0600

    made fixes to the range entity

---
+++ countUpAndDown/main.py
@@ -0,0 +1,9 @@
+#Samuel Andelin, Count Up and Down Proficiency
+def up():
+    for num in range(0, 21):
+        print(num)
+def down():
+    for i in range(19, -1, -1):
+        print(i)
+up()
+down()
