9e4da796705c2d07c290848872f8bfc5627a8127
Parryhip <samuel.andelin@ucas-edu.net>
2024-10-23 09:29:11 -0600

    found the last bug

--- fixTheGameProficiency/main.py
+++ fixTheGameProficiency/main.py
@@ -8,19 +8,16 @@ def start_game():
     attempts = 0
     game_over = False
     while not game_over:
+        attempts += 1 #The bug was that the computer didn't know when the user's attempts ran out. This is a logic error. If the computer doesn't know if the user has used up all of their attempts, then the user can just spam random numbers with no consequence of losing.
         guess = int(input("Enter your guess: "))#The bug was that the computer can't compare integers and strings. This is a runtime error. If the computer can't compare strings and integers, then it won't know if you are too high, too low, or right.
-        if attempts >= max_attempts:
-            print(f"Sorry, you've used all {max_attempts} attempts. The number was {number_to_guess}.")
-            game_over = True
-        if guess == number_to_guess: 
-            print("Congratulations! You've guessed the number!")
-            game_over = True
+        if guess == number_to_guess:
+            continue  #The bug was that the you can win and lose at the same time if you guess the right number on your last attempt. This is a logic error. If we can win and lose at the same time, there is no point to the last attempt.
         elif guess > number_to_guess: 
             print("Too high! Try again.")
-            attempts += 1 #The bug was that the computer didn't know when the user's attempts ran out. This is a logic error. If the computer doesn't know if the user has used up all of their attempts, then the user can just spam random numbers with no consequence of losing.
         elif guess < number_to_guess: 
             print("Too low! Try again.")  
-            attempts += 1 #Needed to increase the attempts variable to actually control the attempts
-        continue
+        elif attempts >= max_attempts: #The bug was that the computer checked if the user lost before it checked if the user won. This is a logic error. If we lose on our last attempt before we win, then there is again no point to the last attempt.
+            print(f"Sorry, you've used all {max_attempts} attempts. The number was {number_to_guess}.")
+            game_over = True
     print("Game Over. Thanks for playing!")
 start_game()
