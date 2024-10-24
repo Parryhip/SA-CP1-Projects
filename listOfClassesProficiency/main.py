6d18e27b4da8e685978f8840473f0a1986cb4429
Parryhip <samuel.andelin@ucas-edu.net>
2024-10-24 09:16:05 -0600

    made the actual program

---
+++ listOfClassesProficiency/main.py
@@ -0,0 +1,4 @@
+#Samuel Andelin, List of Classes Proficiency
+classes = ["CS 1400", "Drawing CE", "Geography", "Student Council", "Seminary", "English 9H", "Biology"]
+for i in classes:
+    print(i)
