7ee10ddd883afe20fda7567d2782208f23489e18
Parryhip <samuel.andelin@ucas-edu.net>
2024-10-24 09:17:10 -0600

    changed names of stuff

--- listOfClasses/main.py
+++ listOfClasses/main.py
@@ -1,4 +1,4 @@
-#Samuel Andelin, List of Classes Proficiency
+#Samuel Andelin, List of Classes
 classes = ["CS 1400", "Drawing CE", "Geography", "Student Council", "Seminary", "English 9H", "Biology"]
 for i in classes:
     print(i)
