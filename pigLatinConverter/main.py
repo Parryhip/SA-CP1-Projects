a44dcd1533a22de26d630f68f7b9a139b94c8211
Parryhip <samuel.andelin@ucas-edu.net>
2024-10-25 09:53:17 -0600

    changes...

--- pigLatinConverter/main.py
+++ pigLatinConverter/main.py
@@ -5,25 +5,13 @@ def switch(word):
         newword.append(word)
         newword.append("way")
     else:
-        if word.find("a") != -1:
-            index = word.find("a")
-            newword.append(word[index:])
-            newword.append("ay")
-        elif word.find("e") != -1:
-            index = word.find("e")
-            newword.append(word[index:])
-            newword.append("ay")
-        elif word.find("i") != -1:
-            index = word.find("i")
-            newword.append(word[index:])
-            newword.append("ay")
-        elif word.find("o") != -1:
-            index = word.find("o")
-            newword.append(word[index:])
-            newword.append("ay")
-        elif word.find("u") != -1:
-            index = word.find("u")
-            newword.append(word[index:])
+        wordlist = []
+        for i in word:
+            wordlist.append(i)
+        while wordlist[0] != "a" or wordlist[0] != "e" or wordlist[0] != "i" or wordlist[0] != "o" or wordlist[0] != "u":
+            wordlist.append(word[0])
+            del wordlist[0]
+            newword.append(word)
             newword.append("ay")
     return newword
 wordtochange = input("What word should I switch to piglatin?")
