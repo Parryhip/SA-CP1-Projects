c4b53013433775acd2fbb433571e847472da0db4
Parryhip <samuel.andelin@ucas-edu.net>
2024-12-12 09:53:26 -0700

    almost finished plyr turn for combat

--- FINALPROJECT!!!/main.py
+++ FINALPROJECT!!!/main.py
@@ -1,6 +1,7 @@
 #Samuel Andelin, FINAL PROJECT!!!
 import random
 import time
+listofanamotopoeia = ["Bang!", "Whack!", "Kapow!", "Kazoinks!", "Bonk!", "Bazingha!", "Yooooowch!", "Oowie", "Boom! Shackalackalacka!"]
 room1 = {
     'monster_probability' : 0,
     'final_boss' : False,
@@ -77,7 +78,9 @@ plyrstats = {
     'hp' : 100,
     'strength' : 13,
     'speed' : 13,
-    'inventory' : []
+    'potioninventory' : [],
+    'spellinventory' : [],
+    'weaponinventory' : []
 }
 normal_enemy = {
     'hp' : 30,
@@ -88,5 +91,102 @@ final_boss = {
     'hp' : 100,
     'strength' : 18,
     'speed' : 18,
-    'inventory' : []
+    'inventory' : ['fireball', 'healing_potion']
 }
+def play_again():
+    while True:
+        playagain = input("Do you want to play again?(y/n)")
+        if playagain.lower() == "y":
+            return True
+        elif playagain.lower() == "n":
+            return False
+        else:
+            print("Invalid input.")
+            time.sleep(0.5)
+            continue
+def plyrturn():
+    while True:
+        plyraction = input("What do you want to do? 1 for attack with sword, 2 for cast spell, 3 for use potion, and 4 for run away.")
+        if plyraction == "1":
+            print("You attack with your sword.")
+            time.sleep(0.5)
+            print("Rolling die for attack...")
+            time.sleep(1)
+            attack = random.randint(1,20)
+            print("You got a " + attack + ".")
+            time.sleep(0.5)
+            overall_attack = str((plyrstats['strength']//3)+attack)
+            print("With your +"+ str(plyrstats['strength']//3) +" modifier means that you end with a "+overall_attack+".")
+            time.sleep(0.5)
+            print(random.choice[listofanamotopoeia])
+            time.sleep(0.5)
+            print("You hit the monster for "+overall_attack+".")
+            normal_enemy['hp'] -= int(overall_attack)
+            time.sleep(0.5)
+            print("The monster's hp is now "+str(normal_enemy['hp'] +"."))
+            time.sleep(0.5)
+            if normal_enemy['hp'] <= 0:
+                print("You killed the enemy!")
+                return 'enemydead'
+            else:
+                print("Enemy's turn!")
+                return 'enemynotdead'
+        elif plyraction == "2":
+            print("These are the potions that you have:")
+            time.sleep(0.5)
+            print(plyrstats['potioninventory'])
+            time.sleep(0.5)
+            while True:
+                potiontouse = input("What potion do you want to use?(use the exact name of the item) or type exit to exit the potion menu.")
+                if potiontouse in plyrstats['potioninventory']:
+                    print("You use your" + potiontouse + ".")
+                    if potiontouse == "speed potion":
+                        speedpotionindex = plyrstats['potioninventory'].index("speed potion")
+                        plyrstats['potioninventory'].pop(speedpotionindex)
+                        plyrstats['speed'] += 5
+                        return 'enemynotdead'
+                    elif potiontouse == "strength potion":
+                        strengthpotionindex = plyrstats['potioninventory'].index("strength potion")
+                        plyrstats['potioninventory'].pop(strengthpotionindex)
+                        plyrstats['strength'] += 5
+                        return 'enemynotdead'
+                    elif potiontouse == "health potion":
+                        healthpotionindex = plyrstats['potioninventory'].index("health potion")
+                        plyrstats['potioninventory'].pop(healthpotionindex)
+                        plyrstats['hp'] += 20
+                        return 'enemynotdead'
+                elif potiontouse == "exit":
+                    break
+                else:
+                    print("You do not have that potion!")
+                    time.sleep(0.5)
+        elif plyraction == "3":
+            print("These are the spells that you have:")
+            time.sleep(0.5)
+            print(plyrstats['spellinventory'])
+            time.sleep(0.5)
+            while True:
+                spelltouse = input("What potion do you want to use?(use the exact name of the item) or type exit to exit the potion menu.")
+                if spelltouse in plyrstats['spellinventory']:
+                    print("You use your" + spelltouse + ".")
+                    if spelltouse == "fireball":
+                        fireballspellindex = plyrstats['spellinventory'].index("fireball")
+                        plyrstats['spellinventory'].pop(fireballspellindex)
+                        normal_enemy['hp'] -= 20
+                        if normal_enemy['hp'] <= 0:
+                            print("You killed the enemy!")
+                            return 'enemydead'
+                        else:
+                            print("Enemy's turn!")
+                            return 'enemynotdead'
+                    elif spelltouse == "slow spell":
+                        slowspellspellindex = plyrstats['spellinventory'].index("slow spell")
+                        plyrstats['spellinventory'].pop(slowspellspellindex)
+                        normal_enemy['speed'] -= 5
+                        return 'enemynotdead'
+                elif spelltouse == "exit":
+                    break
+                else:
+                    print("You do not have that spell!")
+                    time.sleep(0.5)
+            
